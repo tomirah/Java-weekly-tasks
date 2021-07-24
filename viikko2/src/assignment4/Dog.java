bde1f2219aa2e0e40fe98733dffc5cec3b39e62e
tomirah <87768829+tomirah@users.noreply.github.com>
2021-07-24 11:42:06 +0300

    vko2/t4

---
+++ viikko2/src/assignment4/Dog.java
@@ -0,0 +1,39 @@
+package assignment4;
+
+import java.util.Scanner;
+public class Dog {
+    private String name;
+    private String sentence;
+    private static Scanner sc = new Scanner(System.in);
+    
+    public void setName () {
+        name = sc.nextLine();
+        if (name.trim().isEmpty()){
+            name = "Doge";
+        }
+        System.out.println("Hey, my name is " + name+"!");
+    
+
+    }
+    public void speak() {
+        sentence = sc.nextLine();
+        if (sentence.trim().isEmpty()){
+            sentence = "Much wow!";
+        }
+        /*while (sc.hasNext()) {
+            int s = sc.nextInt();
+            String s = sc.nextString();
+        }
+        sentence = s;
+        if (sentence.trim().isEmpty()){
+            sentence = "Much wow!";
+        }*/
+        System.out.print(name+": "+sentence);
+        
+    }/*
+    public String getName() {
+        return name;
+        System.out.println(name);
+    }*/
+}
+
