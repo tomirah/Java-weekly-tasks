d3e02982a650a39aec11aa4d8a24e5c6790587f6
tomirah <87768829+tomirah@users.noreply.github.com>
2021-07-24 11:48:19 +0300

    vko2/t5

---
+++ viikko2/src/assignment5/Mainclass.java
@@ -0,0 +1,19 @@
+package assignment5;
+
+public class Mainclass
+{
+	public static void main(String[] args) {
+		
+		
+		System.out.print("Give a name to the dog:");
+		
+		Dog dog = new Dog();
+		dog.setName();
+		/*Dog dog2 = new Dog();
+		dog1.setName("Rekku");
+		dog2.setName("Musti");
+		dog1.speak("Hau!");*/
+		System.out.print("What does a dog say: ");
+		dog.speak();
+	}
+}
