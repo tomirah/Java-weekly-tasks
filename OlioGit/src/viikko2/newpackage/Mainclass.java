894f8bbf429acfdb1687de7f7e29094df04f787f
tomirah <87768829+tomirah@users.noreply.github.com>
2021-07-24 13:05:09 +0300

    vko3t2

---
+++ OlioGit/src/viikko2/newpackage/Mainclass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package viikko2.newpackage;
+
+/**
+ *
+ * @author 35850
+ */
+public class Mainclass {
+    
+}
