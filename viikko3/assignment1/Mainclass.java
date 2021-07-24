2f1c89da43a888e539af501ca14be84e6aac7695
tomirah <87768829+tomirah@users.noreply.github.com>
2021-07-24 12:06:16 +0300

    vko3/t1

---
+++ viikko3/assignment1/Mainclass.java
@@ -0,0 +1,19 @@
+package assignment1;
+
+public class Mainclass
+{
+	public static void main(String[] args) {
+		
+		BottleDispenser bottledispenser = new BottleDispenser();
+		
+	
+		bottledispenser.addMoney();
+		bottledispenser.buyBottle();
+		bottledispenser.buyBottle();
+		bottledispenser.addMoney();
+		bottledispenser.addMoney();
+		bottledispenser.buyBottle();
+		bottledispenser.returnMoney();
+
+	}
+}
